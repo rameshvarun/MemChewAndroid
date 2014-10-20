197dd9a03f5ffeb55ff1ba9a73f2363c81256539
Varun Ramesh <varun.ramesh@aol.com>
2014-10-20 14:47:00 -0700

    Added export icon script

---
+++ media/export_icon.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+inkscape -f icon.svg -e ../app/src/main/res/drawable-mdpi/ic_launcher.png -w 48 -h 48
+inkscape -f icon.svg -e ../app/src/main/res/drawable-hdpi/ic_launcher.png -w 72 -h 72
+inkscape -f icon.svg -e ../app/src/main/res/drawable-xhdpi/ic_launcher.png -w 96 -h 96
+inkscape -f icon.svg -e ../app/src/main/res/drawable-xxhdpi/ic_launcher.png -w 144 -h 144
