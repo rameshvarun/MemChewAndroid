6d41d6900cfdfdc68e2261dad90d598d033529f0
Varun Ramesh <varun.ramesh@aol.com>
2014-11-07 20:02:13 -0800

    Upvote and downvote icons are now svgs, exported by the script. Fixes issue with #1

--- media/export_svg.sh
+++ media/export_svg.sh
@@ -3,3 +3,6 @@ inkscape -f icon.svg -e ../app/src/main/res/drawable-mdpi/ic_launcher.png -w 48 
 inkscape -f icon.svg -e ../app/src/main/res/drawable-hdpi/ic_launcher.png -w 72 -h 72
 inkscape -f icon.svg -e ../app/src/main/res/drawable-xhdpi/ic_launcher.png -w 96 -h 96
 inkscape -f icon.svg -e ../app/src/main/res/drawable-xxhdpi/ic_launcher.png -w 144 -h 144
+
+inkscape -f down_chevron.svg -e ../app/src/main/res/drawable/down_chevron.png -w 88 -h 60
+inkscape -f up_chevron.svg -e ../app/src/main/res/drawable/up_chevron.png -w 88 -h 60
